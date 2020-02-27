940df133e1ec9d08dad3adf7f585ebfdc79dba26
RD345 <43142825+RD345@users.noreply.github.com>
2020-02-26 21:39:45 -0500

    completed 3_b

--- question_3_b.py
+++ question_3_b.py
@@ -1,2 +1,23 @@
 # Project 1 Question 3 part b code - Python
 # Created by Ryan Doherty
+
+# (b) (3 points) (You must submit code for this question!) Implement a function getSortedArray(n)
+# where the output is an array of size n. The 0th element should be equal to n, the
+# 1st element should be equal to n-1, and so on.
+
+def getSortedArray(n):
+    arr = []
+ 
+    for i in range(0, n):
+        arr.append(i + 1)
+
+    arr.reverse()
+
+    return arr
+
+def printArray(arr):
+    for i in arr:
+        print(i, end=' ')
+
+arr = getSortedArray(20)
+printArray(arr)
