f18be64cb58b1fe42b9e43175ff8b368ab4d12b9
RD345 <rd345@njit.edu>
2020-03-04 12:29:26 -0500

    Cleaned up 6_a

--- question_6_a.py
+++ question_6_a.py
@@ -2,7 +2,6 @@
 # Created by Ryan Doherty
 # (a)	(5 points) (You must submit code for this question!) Modify your iterative implementations of your methods in AVLTree and BinarySearchTree by keeping track of how many times you traverse one level down in the tree. In other words, if you go from a node to its child, add 1 to the counter.
 
-import timeit
 import time
 import question_1_d as BST # Iteritive BST
 import test as AVL # AVL Tree
@@ -13,22 +12,18 @@ def createTree(tree):
         tree.insertIter(n)
     return tree
 
-
-arr = RA.getRandomArray(10000)
-# avl = AVL.Node(None)
-# bst = BST.Node(None)
+beforeTime = time.perf_counter()
+arr = RA.getRandomArray(30000)
+print("Time to generate array:", time.perf_counter() - beforeTime)
 
 beforeTime = time.perf_counter()
 # avl = createTree(AVL.Node(None))
-avl_time = time.perf_counter() - beforeTime
+print("The time to complete the AVL is:", time.perf_counter() - beforeTime)
 
 beforeTime = time.perf_counter()
 bst = createTree(BST.Node(None))
-bst_time = time.perf_counter() - beforeTime
+print("The time to complete the BST is:", time.perf_counter() - beforeTime)
 
 
 # print(arr)
 # bst.printTree()
-
-print("The time to complete the BST is:", bst_time)
-print("The time to complete the AVL is:", avl_time)
