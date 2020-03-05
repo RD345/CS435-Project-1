a82a46fe1212f0fab109aae0379b7a1256beee9a
RD345 <rd345@njit.edu>
2020-03-04 22:01:22 -0500

    Completed 5-a and 5-c

--- question_5_a.py
+++ question_5_a.py
@@ -1,15 +1,18 @@
 # Project 1 Question 5-a - Python
 # Created by Ryan Doherty
-# (a)	(5 points) (You must submit code for this question!) Use your recursive implementation of your BST and your iterative implementation of your AVL Tree from Parts 1 and 2 to construct trees using getRandomArray(10,000). Both trees must be made from the same array. In other words, do not call the method twice - store the output of the method from getRandomArray(10,000) once and use it to construct both trees.
+# (a)	(5 points)
+# Use your recursive implementation of your BST and your iterative implementation of your AVL Tree from Parts 1 and 2 to construct trees using getRandomArray(10,000). 
+
 import question_1_c as BST # Recursive BST
 import question_4_c as AVL # AVL Tree
 import question_3_a as RA # getRandomArray
 
-arr = RA.getRandomArray(100)
+arr = RA.getRandomArray(1000000)
 AVL = AVL.Node(None)
 BST = BST.Node(None)
 
 for n in arr:
     BST.insertRec(n)
-print(arr)
-BST.printTree()
+    AVL.insertIter(n)
+
+print("Trees sucessfully created!")
