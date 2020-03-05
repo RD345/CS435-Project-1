b1e2b0cda30daba0eedf096aafdf380b4cbd6436
RD345 <rd345@njit.edu>
2020-03-04 23:05:30 -0500

    Finished 7-a, but delete has bug

--- question_7_a.py
+++ question_7_a.py
@@ -5,26 +5,43 @@
 
 import time
 import question_1_d as BST # Iteritive BST
-import question_4_c as AVL # AVL Tree
-import question_3_a as RA # getRandomArray
+import question_4_c as AVL # Iteritive AVL Tree
+import question_3_a as RA  # getRandomArray
 
 def createTree(tree):
     for n in arr:
         tree.insertIter(n)
     return tree
 
-beforeTime = time.perf_counter()
-arr = RA.getRandomArray(30000)
-print("Time to generate array:", time.perf_counter() - beforeTime)
+def deleteTree(tree):
+    for n in arr:
+        tree.deleteIter(n)
+    return tree
+
+avl_time = []
+bst_time = []
 
 beforeTime = time.perf_counter()
-avl = createTree(AVL.Node(None))
-print("The time to complete the AVL is:", time.perf_counter() - beforeTime)
+arr = RA.getRandomArray(10000)
+print("Time to generate array:", time.perf_counter() - beforeTime, "ms")
+
 
 beforeTime = time.perf_counter()
 bst = createTree(BST.Node(None))
-print("The time to complete the BST is:", time.perf_counter() - beforeTime)
+bst_time.append(1000 * (time.perf_counter() - beforeTime))
+print("The time to complete the BST is:", bst_time[0], "ms")
 
+beforeTime = time.perf_counter()
+avl = createTree(AVL.Node(None))
+avl_time.append(1000 * (time.perf_counter() - beforeTime))
+print("The time to complete the AVL is:", avl_time[0], "ms")
 
-# print(arr)
-# bst.printTree()
+beforeTime = time.perf_counter()
+bst = deleteTree(BST.Node(None))
+bst_time.append(1000 * (time.perf_counter() - beforeTime))
+print("The time to delete the BST is:", bst_time[1], "ms")
+
+beforeTime = time.perf_counter()
+avl = deleteTree(AVL.Node(None))
+avl_time.append(1000 * (time.perf_counter() - beforeTime))
+print("The time to delete the AVL is:", avl_time[1], "ms")
