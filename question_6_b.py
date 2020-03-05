526ce426bb0cc37d8f3d0c05bf70e57448d75a1f
RD345 <rd345@njit.edu>
2020-03-04 21:33:48 -0500

    Completed 6-b and 6-c

--- question_6_b.py
+++ question_6_b.py
@@ -1,4 +1,20 @@
 # Project 1 Question 6-b - Python
 # Created by Ryan Doherty
 # (b)	(5 points) (You must submit code for this question!) Construct a BST and AVLTree iteratively using getRandomArray(10000). Compare how many levels we have to traverse in the two trees. You can include a screenshot of your code’s output or write it out by hand.
-import question_6_a
+
+import question_1_d as BST # Iteritive BST
+import question_4_c as AVL # AVL Tree
+import question_3_a as RA # getRandomArray
+
+def createTree(tree):
+    for n in arr:
+        tree.insertIter(n)
+    return tree
+
+arr = RA.getRandomArray(10000)
+
+avl = createTree(AVL.Node(None))
+print("The traversals of the AVL is:", avl.traversals)
+
+bst = createTree(BST.Node(None))
+print("The traversals of the BST is:", bst.traversals)
--- question_6_c.py
+++ question_6_c.py
@@ -1,3 +1,20 @@
 # Project 1 Question 6-c - Python
 # Created by Ryan Doherty
 # (c)	(5 points) (You must submit code for this question!) Construct a BST and AVLTree iteratively using getSortedArray(10000). Compare how many levels we have to traverse in the two trees. You can include a screenshot of your code’s output or write it out by hand.
+
+import question_1_d as BST # Iteritive BST
+import question_4_c as AVL # AVL Tree
+import question_3_b as SA # getSortedArray
+
+def createTree(tree):
+    for n in arr:
+        tree.insertIter(n)
+    return tree
+
+arr = SA.getSortedArray(10000)
+
+avl = createTree(AVL.Node(None))
+print("The traversals of the AVL is:", avl.traversals)
+
+bst = createTree(BST.Node(None))
+print("The traversals of the BST is:", bst.traversals)
